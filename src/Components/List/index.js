d90b3a846ea36fa9c24b9810d9a28d75103dbbae
Sena Oz <tazeyta@gmail.com>
2022-04-12 08:54:31 +0300

    12 April 22

---
+++ src/Components/List/index.js
@@ -0,0 +1,9 @@
+export default function List() {
+  return (
+    <section className="weather my-4 grid grid-cols-2 gap-4">
+      <div className="border p-5 text-center rounded">X</div>
+      <div className="border p-5 text-center rounded">Y</div>
+      <div className="border p-5 text-center rounded">Z</div>
+    </section>
+  );
+}
