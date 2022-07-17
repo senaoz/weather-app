68f5c6ca828f143c3bf344f2f7c680da5a03f403
Sena Oz <tazeyta@gmail.com>
2022-07-17 17:08:13 +0300

    Dark Mode Switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import SearchBox from "./Components/SearchBox";
 import List from "./Components/List";
 import { LocProvider } from "./Context/LocationProvider";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import { useState } from "react";
 
 function App() {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = (checked) => {
+    setDarkMode(checked);
+    localStorage.setItem("theme", checked ? "dark" : "light");
+  };
+
   return (
     <LocProvider>
-      <SearchBox />
+      <div className="flex items-center">
+        <div className="flex-1 mr-4">
+          <SearchBox />
+        </div>
+
+        <DarkModeSwitch checked={isDarkMode} onChange={toggleDarkMode} />
+      </div>
       <List></List>
     </LocProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-  darkMode: "media",
+  darkMode: "class",
 };
